3bdd84e57cfcf163447484b043606a9573b39ca1
Raul de Melo <melo.raulf@gmail.com>
2023-03-30 11:33:26 +0200

    finishes the demo

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,10 @@ import vercel from "@astrojs/vercel/serverless";
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: vercel(),
+  adapter: vercel({
+    includeFiles: [
+      // Adding this fixes the problem
+      // "./node_modules/.pnpm/vscode-oniguruma@1.7.0/node_modules/vscode-oniguruma/release/onig.wasm",
+    ],
+  }),
 });
